3b9fe2fc761599a691dbc1f83c32fac9cd5d0658
liulongzhenhai <longzhenhai.liu@qyer.com>
2015-11-11 11:48:48 +0800

    add rxjava

--- app/src/main/java/com/niupme/photo/purephoto/MainActivity.kt
+++ app/src/main/java/com/niupme/photo/purephoto/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.niupme.photo.purephoto
 
 import android.app.Activity
+import android.os.Bundle
 
 public class MainActivity : Activity() {
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
